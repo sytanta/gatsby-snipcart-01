99d43e4fa74e6a3fb9f5c7974f3e40dcb07cf6fb
Tan <sytanta@yahoo.com>
2020-08-26 07:14:00 +0700

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Regular Joe's`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Snipcart store demo`,
+    description: `A demo store built with Gatsby with Snipcart integrated`,
+    author: `@sytanta`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
