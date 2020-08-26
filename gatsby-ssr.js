131adc507e807ac6281683a35c71566c72189d47
Tan <sytanta@yahoo.com>
2020-08-26 22:53:46 +0700

    Remove gatsby-plugin-snipcart plugin

---
+++ gatsby-ssr.js
@@ -0,0 +1,21 @@
+const React = require("react")
+
+exports.onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
+  setHeadComponents([
+    <link rel="preconnect" href="https://app.snipcart.com" />,
+    <link rel="preconnect" href="https://cdn.snipcart.com" />,
+    <link
+      rel="stylesheet"
+      href="https://cdn.snipcart.com/themes/v3.0.20/default/snipcart.css"
+    />,
+  ])
+
+  setPostBodyComponents([
+    <div
+      hidden
+      id="snipcart"
+      data-api-key="MGY4OTM1MzgtYzM0ZS00M2ZkLTg5OWQtNTVhZWZhMzhiMzIxNjM3MzM3NzIxNjY5NDk3NTY1"
+    ></div>,
+    <script src="https://cdn.snipcart.com/themes/v3.0.20/default/snipcart.js"></script>,
+  ])
+}
