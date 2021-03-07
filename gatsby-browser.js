808f4b422a8d2479378260637cd1a7b2dde70778
Tan <sytanta@yahoo.com>
2021-03-07 23:16:24 +0700

    Not scroll to top on page update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,32 +6,7 @@ export const wrapPageElement = ({ element, props }) => (
     <Layout {...props}>{element}</Layout>
 )
 
-export const shouldUpdateScroll = ({
-    routerProps: { location },
-    getSavedScrollPosition,
-}) => {
-    // transition duration from `layout.js` * 1000 to get time in ms
-    // * 2 for exit + enter animation
-    const TRANSITION_DELAY = 0.4 * 1000 * 2
-
-    // if it's a "normal" route
-    if (location.action === 'PUSH') {
-        window.setTimeout(() => {
-            scrollToPosition(0, 0)
-        }, TRANSITION_DELAY)
-    }
-
-    // if we used the browser's forwards or back button
-    else {
-        const savedPosition = getSavedScrollPosition(location) || [0, 0]
-
-        window.setTimeout(() => {
-            scrollToPosition(...savedPosition)
-        }, TRANSITION_DELAY)
-    }
-
-    return false
-}
+export const shouldUpdateScroll = () => false
 
 const scrollToPosition = (x, y) => {
     const c = document.documentElement.scrollTop || document.body.scrollTop
@@ -44,13 +19,3 @@ const scrollToPosition = (x, y) => {
         window.requestAnimationFrame(scrollToPosition.bind(null, x, y))
     }
 }
-
-// export function onRouteUpdate() {
-//     if (window.netlifyIdentity) {
-// Init netlify identity
-// window.netlifyIdentity.init({
-//     container: 'body',
-//     locale: 'en',
-// })
-// }
-// }
